6ab332706bffe2313a1c192de361da2678281e15
DDA209 <didier.jeux1@gmail.com>
2020-07-22 16:55:33 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Bootstrap from "bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      activeTab: 'add',
+      items: [],
+    };
+
+  }
+
+  
+
+  render(Add, List, Pay) {
+    return (
+      <div>
+
+        <h1>
+          Bakery
+        </h1>
+
+        <div>
+          add
+        </div>
+
+        <div>
+          list
+        </div>
+
+        <div>
+          pay
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
